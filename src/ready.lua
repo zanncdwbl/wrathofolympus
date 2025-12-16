a8a5cdfbce9117998b240cedbc1eae066c1a27ed
WistitiPNF-404 <guilmario26@hotmail.com>
2025-12-16 13:44:08 -0500

    2025-12-16 Wrath of Zeus, Heph and Aphro

--- src/ready.lua
+++ src/ready.lua
@@ -19,6 +19,8 @@ modutil.mod.Path.Wrap("SetupMap", function(base, ...)
 end)
 
 local TraitTextFile = rom.path.combine(rom.paths.Content, "Game/Text/en/TraitText.en.sjson")
+local HelpTextFile = rom.path.combine(rom.paths.Content, "Game/Text/en/HelpText.en.sjson")
+local PlayerProjectilesFile = rom.path.combine(rom.paths.Content, "Game/Projectiles/PlayerProjectiles.sjson")
 local Order = { "Id", "InheritFrom", "DisplayName", "Description" }
 
 local not_public = {}
@@ -52,12 +54,80 @@ mod.AresWrathBoonPicked_Text = sjson.to_object({
     Description = "{#UpgradeFormat}{$TooltipData.ExtractData.CurrentMultiplier:P}",
 }, Order)
 
+mod.HephWrathBoon_Text = sjson.to_object({
+    Id = "BlastRevengeStatDisplay1",
+    InheritFrom = "BaseStatLine",
+    DisplayName = "{!Icons.Bullet}{#PropertyFormat}Armor Gained Now:",
+    Description = "{#UpgradeFormat}+{$TooltipData.ExtractData.TooltipAmount}",
+}, Order)
+
+mod.ZeusWrathBoon_Text = sjson.to_object({
+    Id = "BlitzVengeanceStatDisplay1",
+    InheritFrom = "BaseStatLine",
+    DisplayName = "{!Icons.Bullet}{#PropertyFormat}Double Strike Chance:",
+    Description = "{#UpgradeFormat}{$TooltipData.StatDisplay1}",
+}, Order)
+
+mod.DemeterWrathBoon_Text = sjson.to_object({
+    Id = "FrostbiteBurstStatDisplay1",
+    InheritFrom = "BaseStatLine",
+    DisplayName = "{!Icons.Bullet}{#PropertyFormat}Frostbite Damage:",
+    Description = "{#UpgradeFormat}{$TooltipData.StatDisplay1} {#Prev}{#ItalicFormat}(per 1 Sec.)",
+}, Order)
+
+mod.AphroWrathBoon_Text = sjson.to_object({
+    Id = "BonusHeartthrobDamageStatDisplay1",
+    InheritFrom = "BaseStatLine",
+    DisplayName = "{!Icons.Bullet}{#PropertyFormat}Bonus Heartthrob Damage:",
+    Description = "{#UpgradeFormat}{$TooltipData.StatDisplay1}",
+}, Order)
+
 sjson.hook(TraitTextFile, function(data)
     table.insert(data.Texts, mod.HeraRandomCurseBoon_Text)
 	table.insert(data.Texts, mod.PoseidonWrathBoon_Text)
 	table.insert(data.Texts, mod.AresWrathBoon_Text)
 	table.insert(data.Texts, mod.AresWrathBoonPicked_Text)
+	table.insert(data.Texts, mod.HephWrathBoon_Text)
+	table.insert(data.Texts, mod.ZeusWrathBoon_Text)
+	table.insert(data.Texts, mod.DemeterWrathBoon_Text)
+	table.insert(data.Texts, mod.AphroWrathBoon_Text)
 end)
+
+mod.ZeusWrathBoon_CombatText = sjson.to_object({
+	Id = "ZeusWrath_CombatText",
+	DisplayName = "{#CombatTextHighlightFormat}{$TempTextData.BoonName} {#Prev}{$TempTextData.Amount}x!",
+}, Order)
+
+sjson.hook(HelpTextFile, function(data)
+	table.insert(data.Texts, mod.ZeusWrathBoon_CombatText)
+end)
+
+--[[mod.FrostbiteBurst_Data = sjson.to_object({
+	Name = "FrostbiteProjectile",
+	InheritFrom = "1_BaseProjectile",
+	DetonateFx = "RadialNovaDemeter",
+	Type = "INSTANT",
+	Fuse = 0.1,
+	Range = 0,
+	Damage = 75,
+	DamageRadius = 220.0,
+	DamageRadiusScaleY = 0.6,
+	DamageRadiusScaleX = 1.1,
+	AutoAdjustForTarget = false,
+	UseVulnerability = false,
+	NumPenetrations = 999,
+	IgnoreDodge = true,
+	SpawnRadius = 0,
+	Speed = -100,
+	UseStartLocation = true,
+	DetonateLineOfSight = true,
+	CanHitWithoutDamage = true,
+	SilentImpactOnInvulnerable = true,
+}, Order)
+
+sjson.hook(PlayerProjectilesFile, function(data)
+	table.insert(data.Texts, mod.FrostbiteBurst_Data)
+end)]]--
 
 game.TraitData.WrathTrait = {
     BlockStacking = true,
@@ -67,7 +137,7 @@ game.TraitData.WrathTrait = {
     CustomRarityColor = Color.BoonPatchHeroic,
     InfoBackingAnimation = "BoonSlotHeroic",
     UpgradeChoiceBackingAnimation = "BoonSlotHeroic",
-    Frame = "Untiy",
+    Frame = "Unity",
     RarityLevels = {
         Legendary = {
             MinMultiplier = 1,
@@ -75,10 +145,6 @@ game.TraitData.WrathTrait = {
         },
     },
 }
-
---[[sjson.hook(TraitData, function(data)
-	table.insert(data.WeaponRarityUpgradeOrder, mod.WrathTrait_Rarity)
-end)]]--
 
 --[[ 
 uid, internal, charactername ,legendary, rarity, slot, blockstacking,  statlines, extractval, elements, displayName
@@ -91,7 +157,7 @@ gods.CreateBoon({
     isLegendary = false,
 	InheritFrom = 
 	{
-		Elements = "FireBoon",
+		"FireBoon",
 	},
     addToExistingGod = true,
 
@@ -110,11 +176,6 @@ gods.CreateBoon({
 	},	
 
 	ExtractValues = { 
-		--[[{
-			Key = "ReportedBonusOlympianDamage",
-			ExtractAs = "TooltipDamageBonus",
-			Format = "PercentDelta",
-		},]]--
 		{
 			Key = "ReportedCurseCount",
 			ExtractAs = "CurseCount",
@@ -274,17 +335,18 @@ gods.CreateBoon({
     pluginGUID = _PLUGIN.guid,
     characterName = "Poseidon",
 	internalBoonName = "PoseidonWrathBoon",
-    isLegendary = true,
+    isLegendary = false,
 	InheritFrom = 
 	{
-		Elements = "WaterBoon",
+		"WrathTrait",
+		"WaterBoon",
 	},
     addToExistingGod = true,
 	reuseBaseIcons = true,
     BlockStacking = true,
 
     displayName = "Wrath of Poseidon",
-    description = "Your splash effects fire your effects from {$TraitData.OmegaPoseidonProjectileBoon.Name} with greater {$Keywords.BaseDamage} at no extra cost.",
+    description = "Your splash effects fire your waves from {$TraitData.OmegaPoseidonProjectileBoon.Name} with more {$Keywords.BaseDamage} at no extra cost.",
 	StatLines = { "BonusOceanSwellStatDisplay1" },
 	requirements =
 	{
@@ -325,10 +387,11 @@ gods.CreateBoon({
     pluginGUID = _PLUGIN.guid,
     characterName = "Ares",
 	internalBoonName = "AresWrathBoon",
-    isLegendary = true,
+    isLegendary = false,
 	InheritFrom = 
 	{
-		Elements = "EarthBoon",
+		"WrathTrait",
+		"EarthBoon",
 	},
     addToExistingGod = true,
 	reuseBaseIcons = true,
@@ -382,6 +445,330 @@ gods.CreateBoon({
             ReportValues = { ReportedPlasmaCritMultiplier = "IncreasingPlasmaCritChance" },
         },
     },
+})
+
+gods.CreateBoon({
+    pluginGUID = _PLUGIN.guid,
+    characterName = "Hephaestus",
+	internalBoonName = "HephWrathBoon",
+    isLegendary = false,
+	InheritFrom = 
+	{
+		"WrathTrait",
+		"CostumeTrait",
+		"FireBoon",
+	},
+    addToExistingGod = true,
+	reuseBaseIcons = true,
+    BlockStacking = true,
+
+    displayName = "Explosive Plating",
+    description = "After you take damage while having {!Icons.ArmorTotal}, create a blast that deals {$TooltipData.ExtractData.Damage} damage in an area.",
+	StatLines = { "BlastRevengeStatDisplay1" },
+	requirements =
+	{
+		OneFromEachSet =
+		{
+			{ "HephaestusWeaponBoon", "HephaestusSpecialBoon", "HephaestusSprintBoon" },
+			{ "HeavyArmorBoon", "ArmorBoon", "EncounterStartDefenseBuffBoon" },
+			{ "HephaestusManaBoon", "ManaToHealthBoon" },
+		},
+	},
+    flavourText = "The roughest hands can often be the ones to produce the most immaculate results.",
+    boonIconPath = "GUI\\Screens\\BoonIcons\\Hephaestus_29",
+    
+	ExtractValues =
+	{
+		{
+			Key = "ReportedBlastDamageMultiplier",
+			ExtractAs = "Damage",
+			Format = "MultiplyByBase",
+			BaseType = "Projectile",
+			BaseName = "MassiveSlamBlast",
+			BaseProperty = "Damage",
+			SkipAutoExtract = true,
+			DecimalPlaces = 1,
+		},
+		{
+			Key = "ReportedExtraArmor",
+			ExtractAs = "TooltipAmount",
+		},
+	},
+
+	ExtraFields = 
+	{
+		Frame = "Unity",
+		Invincible = true,
+		OnSelfDamagedFunction =
+		{
+			Name = "rom.mods." .. _PLUGIN.guid .. ".not.HephRetaliate",
+			FunctionArgs = 
+			{
+				ProjectileName = "MassiveSlamBlast",
+				Cooldown = 0.4,
+				BlastDelay = 0.08,
+				DamageMultiplier = 3.0,
+				ReportValues = 
+				{
+					ReportedBlastDamageMultiplier = "DamageMultiplier",
+				}
+			}
+		},
+		AcquireFunctionName = "HeavyArmorInitialPresentation",
+		SetupFunctions =
+		{
+			{
+				Name = "CostumeArmor",
+				Args = 
+				{
+					Source = "Tradeoff",
+					Delay = 0.75,
+					BaseAmount =
+					{
+						BaseValue = 100,
+					},
+					ReportValues = 
+					{
+						ReportedExtraArmor = "BaseAmount",
+					},
+				},
+			},
+		},
+    },
+})
+
+gods.CreateBoon({
+    pluginGUID = _PLUGIN.guid,
+    characterName = "Zeus",
+	internalBoonName = "ZeusWrathBoon",
+    isLegendary = false,
+	InheritFrom = 
+	{
+		"WrathTrait",
+		"AirBoon",
+	},
+    addToExistingGod = true,
+	reuseBaseIcons = true,
+    BlockStacking = true,
+
+    displayName = "Wrath of Zeus",
+    description = "Activating {$Keywords.Echo} on foes strikes them with {#BoldFormatGraft}{$TooltipData.ExtractData.BoltsNumber} {#Prev}lightning bolts, each dealing {#BoldFormatGraft}{$TooltipData.ExtractData.WrathBoltDamage} {#Prev}damage.",
+	StatLines = { "BlitzVengeanceStatDisplay1" },
+	requirements =
+	{
+		OneFromEachSet =
+		{
+			{ "SuperSacrificeBoonHera" },
+			{ "ZeusWeaponBoon", "ZeusSpecialBoon" },
+		},
+	},
+    flavourText = "The lightning bolt forever remains a symbol of the impulsive power of the Lord of Olympus.",
+    boonIconPath = "GUI\\Screens\\BoonIcons\\Zeus_33",
+
+	ExtractValues =
+	{
+		{
+			Key = "ReportedBoltChance",
+			ExtractAs = "DoubleChance",
+			Format = "LuckModifiedPercent",
+		},
+		{
+			Key = "ReportedMinStrikes",
+			ExtractAs = "BoltsNumber",
+			SkipAutoExtract = true,
+		},
+		{
+			Key = "BoltDamage",
+			ExtractAs = "WrathBoltDamage",
+			SkipAutoExtract = true,
+		},
+		{
+			ExtractAs = "EchoDuration",
+			SkipAutoExtract = true,
+			External = true,
+			BaseType = "EffectData",
+			BaseName = "DamageEchoEffect",
+			BaseProperty = "Duration",
+		},
+		{
+			ExtractAs = "EchoThreshold",
+			SkipAutoExtract = true,
+			External = true,
+			BaseType = "EffectData",
+			BaseName = "DamageEchoEffect",
+			BaseProperty = "DamageThreshold",
+		},
+	},
+
+	ExtraFields = 
+	{
+		BoltDamage = 100, -- used for description only
+		OnEnemyDamagedAction =
+		{
+			Name = "ZeusWrath",
+			Args = 
+			{
+				ProjectileName = "ZeusRetaliateStrike",
+				DoubleBoltChance = 0.4,
+				MinStrikes = 3,
+				MaxStrikes = 
+				{ 
+					BaseValue = 6,
+					MinValue = 3,
+					IdenticalMultiplier = 
+					{
+						Value = -0.5,
+					},
+				},
+				ReportValues = 
+				{ 
+					ReportedMaxStrikes = "MaxStrikes",
+					ReportedMinStrikes = "MinStrikes",
+					ReportedBoltChance = "DoubleBoltChance",
+				},
+			},
+		},
+	},
+})
+
+--[[gods.CreateBoon({
+    pluginGUID = _PLUGIN.guid,
+    characterName = "Demeter",
+	internalBoonName = "DemeterWrathBoon",
+    isLegendary = false,
+	InheritFrom = 
+	{
+		"WrathTrait",
+		"WaterBoon",
+	},
+    addToExistingGod = true,
+	reuseBaseIcons = true,
+    BlockStacking = true,
+
+    displayName = "Wrath of Demeter",
+    description = "After the {$Keywords.Root} duration on your foes expires, they suffer from Frostbite.",
+	StatLines = { "FrostbiteBurstStatDisplay1" },
+	requirements =
+	{
+		OneFromEachSet =
+		{
+			{ "DemeterWeaponBoon", "DemeterSpecialBoon", "DemeterCastBoon" },
+			{ "DemeterSprintBoon", "CastNovaBoon" },
+			{ "SlowExAttackBoon", "CastAttachBoon", "RootDurationBoon" },
+		},
+	},
+    flavourText = "Life is resilient, and can take root even in cold, harsh environments... but only to a point.",
+    boonIconPath = "GUI\\Screens\\BoonIcons\\Demeter_32",
+    
+	ExtractValues =
+	{
+		{
+			Key = "ReportedFrostbiteMutiplier",
+			ExtractAs = "Damage",
+			Format = "MultiplyByBase",
+			BaseType = "Projectile",
+			BaseName = "FrostbiteProjectile",
+			BaseProperty = "Damage",
+		},
+		{
+			ExtractAs = "ChillDuration",
+			SkipAutoExtract = true,
+			External = true,
+			BaseType = "EffectData",
+			BaseName = "ChillEffect",
+			BaseProperty = "Duration",
+		},
+		{
+			ExtractAs = "ChillActiveDuration",
+			SkipAutoExtract = true,
+			External = true,
+			BaseType = "EffectData",
+			BaseName = "ChillEffect",
+			BaseProperty = "ActiveDuration",
+		},
+	},
+
+	ExtraFields = 
+	{
+		Frame = "Unity",
+		OnEnemyDamagedAction =
+		{
+			Name = "FrostbiteDamage",
+			Args =
+			{
+				ProjectileName = "FrostbiteEffect",
+				FreezeTimeScale = 1,
+				FrostbiteMultiplier =
+				{
+					BaseValue = 1,
+				},
+			},
+			ReportedValues =
+			{
+				ReportedFrostbiteMultiplier = "FrostbiteMultipier",
+			},
+		},
+    },
+})]]--
+
+gods.CreateBoon({
+    pluginGUID = _PLUGIN.guid,
+    characterName = "Aphrodite",    
+	internalBoonName = "AphroWrathBoon",
+    isLegendary = false,
+	InheritFrom = 
+	{
+		"WrathTrait",
+		"WaterBoon",
+	},
+    addToExistingGod = true,
+	reuseBaseIcons = true,
+	BlockStacking = true,
+
+    displayName = "Lustful Confession",
+    description = "Your {$Keywords.HeartBurstPlural} are stronger and fire your {$Keywords.CastEX} upon striking a foe.",
+	StatLines = { "BonusHeartthrobDamageStatDisplay1" },
+	requirements = 
+	{
+		OneFromEachSet =
+		{
+			{ "AphroditeWeaponBoon", "AphroditeSpecialBoon" },
+			{ "ManaBurstBoon" },
+			{ "HighHealthOffenseBoon", "HealthRewardBonusBoon", "FocusRawDamageBoon" },
+		},
+	},
+	flavourText = "Love and beauty can be so overwhelming as to strike each of the senses numb.",
+    boonIconPath = "GUI\\Screens\\BoonIcons\\Aphrodite_40",
+
+	ExtractValues = { 
+		{
+			Key = "ReportedHeartthrobMultiplier",
+			ExtractAs = "HeartthrobMultiplier",
+			Format = "PercentDelta",
+		},
+		{
+			ExtractAs = "Duration",
+			SkipAutoExtract = true,
+			External = true,
+			BaseType = "ProjectileBase",
+			BaseName = "AphroditeBurst",
+			BaseProperty = "Fuse",
+		},
+	},
+
+	ExtraFields = 
+	{
+		HeartthrobBonusDamageModifiers = 
+		{
+			ValidProjectiles = "AphroditeBurst",
+			HeartthrobBonusMultiplier = 
+			{
+				BaseValue = 1.5,
+			},
+			SourceIsMultiplier = true,
+            ReportValues = { ReportedHeartthrobMultiplier = "HeartthrobBonusMultiplier" },
+        },
+	}
 })
 
 -- Function Library --
@@ -512,6 +899,9 @@ modutil.mod.Path.Wrap("CalculateDoubleDamageChance", function(baseFunc, attacker
 end)
 
 function CalculatePlasmaDoubleDamageChance( attacker, victim, weaponData, triggerArgs )
+	--[[if not HeroHasTrait(gods.GetInternalBoonName("AresWrathBoon")) then
+		return
+	end]]--
 	if attacker ~= nil and attacker.OutgoingDoubleDamageModifiers ~= nil then
 		local appliedEffectTable = {}
 		for i, modifierData in ipairs( attacker.OutgoingDoubleDamageModifiers ) do
@@ -536,7 +926,7 @@ function CalculatePlasmaDoubleDamageChance( attacker, victim, weaponData, trigge
 				if modifierData.IncreasingPlasmaCritChance then
 					local totalPlasma = CurrentRun.CurrentRoom.BloodDropCount * GetTotalHeroTraitValue( "BloodDropMultiplier", { IsMultiplier = true } )
 					addDdMultiplier( modifierData, modifierData.IncreasingPlasmaCritChance * totalPlasma, triggerArgs)
-					modutil.mod.Hades.PrintOverhead("DoubleDamageChance "..(triggerArgs.DdChance))
+					-- modutil.mod.Hades.PrintOverhead("DoubleDamageChance "..(triggerArgs.DdChance))
 				end
 			end
 		end
@@ -551,17 +941,98 @@ modutil.mod.Path.Wrap("FormatExtractedValue", function(baseFunc, value, extractD
 	return baseFunc(value, extractData)
 end)
 
---[[modutil.mod.Hades.PrintOverhead(config.message)
-modutil.mod.Hades.PrintOverhead("Comme les 5 doigts de la main")]]--
-
---[[modutil.mod.Path.Wrap("CalculateDoubleDamageChance", function(baseFunc, attacker, victime, weaponData, triggerArgs)
-	baseFunc(attacker, victim, weaponData, triggerArgs)
-	if modifierData.IncreasingPlasmaCritChance then
-		local totalPlasma = CurrentRun.CurrentRoom.BloodDropCount
-		addDdMultiplier( modifierData, modifdierData.IncreasingPlasmaCritChance * totalPlasma, triggerArgs)
+-- HephaestusWrath custom function
+function not_public.HephRetaliate( unit, args )
+	if not HeroHasTrait(gods.GetInternalBoonName("HephWrathBoon")) then
+		return
 	end
-end)]]--
+	if not unit or unit.SkipModifiers or not CheckCooldown( "HephRetaliate"..unit.ObjectId, args.Cooldown ) then
+		return
+	end
+	if CurrentRun.Hero.HealthBuffer <= 0 then
+		return
+	end
 
+	local blastModifier = GetTotalHeroTraitValue( "MassiveAttackSizeModifier", { IsMultiplier = true })
+	waitUnmodified( args.BlastDelay or 0 )
+	if unit then
+		CreateProjectileFromUnit({ Name = args.ProjectileName, Id = CurrentRun.Hero.ObjectId, DestinationId = unit.ObjectId, DamageMultiplier = args.DamageMultiplier, BlastRadiusModifier = blastModifier, FireFromTarget = true })
+		if unit.IsDead then
+			CreateAnimation({ Name = "HephMassiveHit", DestinationId = unit.ObjectId })
+		end
+	else
+		CreateProjectileFromUnit({ Name = args.ProjectileName, Id = CurrentRun.Hero.ObjectId, ProjectileDestinationId = args.ProjectileId, DamageMultiplier = args.DamageMultiplier, BlastRadiusModifier = blastModifier, FireFromTarget = true })
+	end
+end
+
+--ZeusWrath custom function
+modutil.mod.Path.Wrap("DamageEnemy", function(baseFunc, victim, triggerArgs)
+	baseFunc(victim, triggerArgs)
+	if not HeroHasTrait(gods.GetInternalBoonName("ZeusWrathBoon")) then
+		return
+	end
+	
+	if (triggerArgs.SourceProjectile == "ZeusEchoStrike") then
+		for _, data in pairs(GetHeroTraitValues("OnEnemyDamagedAction")) do
+			if data.Name == "ZeusWrath" then
+			return ZeusWrath( victim, data.Args )
+			end
+		end
+	end
+end)
+
+function ZeusWrath( unit, args )
+	local strikeCount = args.MinStrikes
+	while RandomChance( args.DoubleBoltChance * GetTotalHeroTraitValue( "LuckMultiplier", { IsMultiplier = true }) ) and strikeCount < args.MaxStrikes do
+		strikeCount = strikeCount + 1
+	end
+	if strikeCount > 1 then
+		thread( InCombatTextArgs, { TargetId = CurrentRun.Hero.ObjectId, Text = "ZeusWrath_CombatText", LuaKey = "TempTextData", ShadowScaleX = 1.1, LuaValue = { Amount = strikeCount, BoonName = gods.GetInternalBoonName("ZeusWrathBoon") }} )
+	end
+	CreateZeusBolt({
+		ProjectileName = args.ProjectileName, 
+		TargetId = unit.ObjectId, 
+		DamageMultiplier = args.DamageMultiplier,
+		Delay = 0.2, 
+		FollowUpDelay = 0.2, 
+		Count = strikeCount
+		})
+end
+
+--DemeterWrath custom functions
+
+--AphroWrath custom functions
+modutil.mod.Path.Wrap("DamageEnemy", function(baseFunc, victim, triggerArgs)
+	baseFunc(victim, triggerArgs)
+	if not HeroHasTrait(gods.GetInternalBoonName("AphroWrathBoon")) then
+		return
+	end
+	local graphic = nil
+	local count = 1
+	if (triggerArgs.SourceProjectile == "AphroditeBurst") then
+		local weaponName = "WeaponCast"
+		local projectileName = "ProjectileCast"
+		local derivedValues = GetDerivedPropertyChangeValues({
+			ProjectileName = projectileName,
+			WeaponName = weaponName,
+			Type = "Projectile",
+		})
+		derivedValues.ThingPropertyChanges = derivedValues.ThingPropertyChanges or {}
+		derivedValues.ThingPropertyChanges.Graphic = "null"
+		local projectileId = CreateProjectileFromUnit({ WeaponName = weaponName, Name = projectileName, Id = CurrentRun.Hero.ObjectId, DestinationId = CurrentRun.Hero.ObjectId, FireFromTarget = true, 
+			DataProperties = derivedValues.PropertyChanges, ThingProperties = derivedValues.ThingPropertyChanges })
+				ArmAndDetonateProjectiles({ Ids = { projectileId }})
+		return
+	end
+end)
+
+modutil.mod.Path.Wrap("CreateProjectileFromUnit", function (baseFunc, args)
+	if args.Name == "AphroditeBurst" then
+		local HeartthrobWrathMultiplier = GetTotalHeroTraitValue("ReportedHeartthrobMultiplier", { IsMultiplier = true })
+		args.DamageMultiplier = args.DamageMultiplier * HeartthrobWrathMultiplier
+	end
+	return baseFunc(args)
+end)
 
 --[[
 	AirBoon = 
